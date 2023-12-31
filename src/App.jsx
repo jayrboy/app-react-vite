3b4170e15bdde9d0aa5b3b0862c89758a151c8e4
jayrboy <j.jayox_@hotmail.com>
2023-12-31 21:17:01 +0700

    Add Image logo
    Add Image logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Item from "./components/Item";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="logo" />
       <h2>React + Vite 2023</h2>
       <Item />
       <Item />
